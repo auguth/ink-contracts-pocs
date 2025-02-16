3739748052538a5312d8eaf01b4b8a111a8fff23
Corinthian <114797914+I-Corinthian@users.noreply.github.com>
2025-02-16 23:29:53 +0530

    Update lib.rs

--- validator_reward_contract/lib.rs
+++ validator_reward_contract/lib.rs
@@ -27,7 +27,7 @@ mod rewardclimer {
         }
 
         #[ink(message)]
-        pub fn claim(    &mut self,
+        pub fn claim( &mut self,
             owner: AccountId,
             delegate_to: AccountId,
             delegate_at: BlockNumber,
@@ -39,14 +39,17 @@ mod rewardclimer {
             
             if delegate_to == self.owner{
                 let current_block = self.env().block_number();
-                let range = current_block - self.last_claim_block.get(&caller).unwrap_or(0);
-                if range < 10 || self.last_claim_block.get(&caller).unwrap_or(0) == 0{
+                let range = current_block.checked_sub(self.last_claim_block.get(&caller).unwrap_or(0));
+                if range > Some(10) || self.last_claim_block.get(&caller).unwrap_or(0) == 0{
                     self.last_claim_block.insert(caller, &current_block);
-                    Self::env().transfer(owner, self.claim_amount);
+                    let claim_amount_in_smallest_units = self.claim_amount.checked_mul(1_000_000_000_000).expect("Overflow during multiplication");
+                    self.env().transfer(owner, claim_amount_in_smallest_units).expect("Transfer failed");
                 }
 
             }
         }
     }
 }
+
+
 
